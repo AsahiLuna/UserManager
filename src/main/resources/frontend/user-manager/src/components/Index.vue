a46047b1ff42e336e5f1563f06914e8c556e3332
Asahi <542792296@qq.com>
2018-05-08 16:25:40 +0800

    [frontend] change hello vue to index vue

---
+++ src/main/resources/frontend/user-manager/src/components/Index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="Index" id="Index">
+    <h1>{{ msg }}</h1>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Index',
+  data () {
+    return {
+      msg: 'Welcome to Your User Manager'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
