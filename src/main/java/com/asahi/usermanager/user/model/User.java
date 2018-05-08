27956b54f7915f9e682069c3114cf4e6ce2edfa7
Asahi <Asahi@DESKTOP-FQKRFU6>
2018-05-08 10:11:38 +0800

    add some user field

--- src/main/java/com/asahi/usermanager/user/model/User.java
+++ src/main/java/com/asahi/usermanager/user/model/User.java
@@ -1,7 +1,11 @@
 package com.asahi.usermanager.user.model;
 
+import java.util.Date;
+
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+import com.asahi.usermanager.user.util.Constants;
 
 @Document(collection = "mongo_user")
 public class User {
@@ -9,6 +13,10 @@ public class User {
     @Id
     private String id;
     private String name;
+    private Date birthDate;
+    private String gender;
+    private String phoneNumber;
+    private String email;
     
     private boolean isDeleted;
 
@@ -24,6 +32,14 @@ public class User {
         this.id = user.id.isEmpty() ? this.id : user.id;
         this.name = user.name;
         this.isDeleted = user.isDeleted;
+        this.birthDate = user.birthDate;
+        this.email = user.email;
+        if(user.gender.equals(Constants.MALE) || user.gender.equals(Constants.FEMALE)) {
+            this.gender = user.gender;
+        }else {
+            this.gender = Constants.UNKNOWN;
+        }
+        this.phoneNumber = user.phoneNumber;
     }
 
 	public String getId() {
@@ -40,5 +56,24 @@ public class User {
     
     public void setDeleted(boolean isDeleted) {
         this.isDeleted = isDeleted;
+    }
+    
+    public Date getBirthDate() {
+        return birthDate;
+    }
+    
+    
+    public String getGender() {
+        return gender;
+    }
+    
+    
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+    
+    
+    public String getEmail() {
+        return email;
     }
 }
---
+++ src/main/java/com/asahi/usermanager/user/util/Constants.java
@@ -0,0 +1,8 @@
+package com.asahi.usermanager.user.util;
+
+public class Constants {
+    public static final String EMPTY_STRING = "";
+    public static final String MALE = "male";
+    public static final String FEMALE = "female";
+    public static final String UNKNOWN = "unknown";
+}
