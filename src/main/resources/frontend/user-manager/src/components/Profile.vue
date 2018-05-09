18891fc96c3fa72ba9804266d9be706625dff9e1
Asahi <542792296@qq.com>
2018-05-09 13:22:17 +0800

    [frontend] draw profile page

--- src/main/resources/frontend/user-manager/src/components/Profile.vue
+++ src/main/resources/frontend/user-manager/src/components/Profile.vue
@@ -1,20 +1,62 @@
 <template>
-  <div class="profile" id="profile">
-    <h1>{{ msg }}</h1>
-    <button type="button" class="btn btn-primary">Login</button>
+  <div class="profile container" id="profile">
+    <h1 class="text-center">{{ msg }}</h1>
+    <form class="form-horizontal col-sm-8" role="form">
+      <div class="form-group">
+        <label for="name" class="col-sm-2 control-label">Name</label>
+        <div class="col-sm-10">
+          <input type="text" class="form-control" id="name" 
+              placeholder="Please input your name">
+        </div>
+      </div>
+      <div class="form-group">
+        <label for="gender" class="col-sm-2 control-label">Gender</label>
+        <div class="col-sm-10">
+          <input type="text" class="form-control" id="gender" 
+              placeholder="Please select your gender">
+        </div>
+      </div>
+      <div class="form-group">
+        <label for="birth-date" class="col-sm-2 control-label">Birth Date</label>
+        <div class="col-sm-10">
+          <input type="text" class="form-control" id="birth-date" 
+              placeholder="Please select your Birth Date">
+        </div>
+      </div>
+      <div class="form-group">
+        <label for="phone-number" class="col-sm-2 control-label">Phone Number</label>
+        <div class="col-sm-10">
+          <input type="text" class="form-control" id="phone-number" 
+              placeholder="Please input your phone number">
+        </div>
+      </div>
+      <div class="form-group">
+        <label for="email" class="col-sm-2 control-label">Email</label>
+        <div class="col-sm-10">
+          <input type="text" class="form-control" id="email" 
+              placeholder="Please input your Email">
+        </div>
+      </div>
+      <div class="form-group">
+        <div class="col-sm-offset-2 col-sm-10">
+          <button type="submit" class="btn btn-primary">Save</button>
+          <button type="submit" class="btn btn-warning">Cancel</button>
+        </div>
+      </div>
+    </form>
     <router-view/>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'profile',
-  data () {
+  name: "profile",
+  data() {
     return {
-      msg: 'Welcome to Your profile Page'
-    }
+      msg: "Welcome to Your profile Page"
+    };
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
